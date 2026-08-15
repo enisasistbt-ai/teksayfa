6bc515eb9e48a4e137ae42cc501f8c9fda6ac06f
enisasistbt-ai <enisasistbt@gmail.com>
2026-08-15 19:47:19 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    serverComponentsExternalPackages: ["iyzipay"],
     outputFileTracingIncludes: {
       "/api/iyzico/checkout/**": ["./node_modules/iyzipay/lib/resources/**"],
       "/api/iyzico/callback/**": ["./node_modules/iyzipay/lib/resources/**"],
