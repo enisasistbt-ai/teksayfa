16b072f42bc795e943b7e8a17605f18a9c5b8fb0
enisasistbt-ai <enisasistbt@gmail.com>
2026-08-15 19:29:14 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      "/api/iyzico/checkout": ["./node_modules/iyzipay/lib/resources/**/*"],
+      "/api/iyzico/callback": ["./node_modules/iyzipay/lib/resources/**/*"],
+    },
+  },
+};
 
 module.exports = nextConfig;
